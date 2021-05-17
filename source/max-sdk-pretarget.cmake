b40a1f541b6eb61641514135d89c91ee4227f02d
Robert Ramirez <rob@robtherich.org>
2021-05-17 11:27:22 -0400

    force macos 10.11 compatibility

--- source/max-sdk-pretarget.cmake
+++ source/max-sdk-pretarget.cmake
@@ -1,3 +1,7 @@
 set(MAX_SDK_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/max-includes")
 set(MAX_SDK_MSP_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/msp-includes")
 set(MAX_SDK_JIT_INCLUDES "${CMAKE_CURRENT_SOURCE_DIR}/../../max-sdk/source/c74support/jit-includes")
+
+if (APPLE)
+	set(CMAKE_OSX_DEPLOYMENT_TARGET "10.11" CACHE STRING "Minimum OS X deployment version" FORCE)
+endif ()
